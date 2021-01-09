2ed070c2e25f090cd02b8b598777a7ec0be10421
Developer Experience team at Tendermint <hello@tendermint.com>
2021-01-09 12:41:27 +0000

    Initialized with Starport

---
+++ x/policingnetworkcosmos/genesis.go
@@ -0,0 +1,22 @@
+package policingnetworkcosmos
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/mukherjeearnab/policing-network-cosmos/x/policingnetworkcosmos/keeper"
+	"github.com/mukherjeearnab/policing-network-cosmos/x/policingnetworkcosmos/types"
+	// abci "github.com/tendermint/tendermint/abci/types"
+)
+
+// InitGenesis initialize default parameters
+// and the keeper's address to pubkey map
+func InitGenesis(ctx sdk.Context, k keeper.Keeper /* TODO: Define what keepers the module needs */, data types.GenesisState) {
+	// TODO: Define logic for when you would like to initalize a new genesis
+}
+
+// ExportGenesis writes the current store values
+// to a genesis file, which can be imported again
+// with InitGenesis
+func ExportGenesis(ctx sdk.Context, k keeper.Keeper) (data types.GenesisState) {
+	// TODO: Define logic for exporting state
+	return types.NewGenesisState()
+}
